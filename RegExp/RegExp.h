1de50ff5d7f4ed582d3ab4150a211c00c60d1697
Gabriel Vieira <gluisvieira@gmail.com>
2015-12-21 12:54:38 -0200

    Initial Commit

---
+++ RegExp/RegExp.h
@@ -0,0 +1,19 @@
+//
+//  RegExp.h
+//  RegExp
+//
+//  Created by Gabriel Luis Vieira on 12/21/15.
+//  Copyright © 2015 mion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RegExp.
+FOUNDATION_EXPORT double RegExpVersionNumber;
+
+//! Project version string for RegExp.
+FOUNDATION_EXPORT const unsigned char RegExpVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RegExp/PublicHeader.h>
+
+
